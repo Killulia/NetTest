755580b75cac6450439fa693c56516cb5132c76a
Kingwag <631703900@qq.com>
2018-09-10 13:46:38 +0800

    add
    constraint plus

--- app/src/main/java/com/bksx/nettest/activity/OkioActivity.java
+++ app/src/main/java/com/bksx/nettest/activity/OkioActivity.java
@@ -1,13 +1,32 @@
 package com.bksx.nettest.activity;
 
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import com.bksx.nettest.R;
+import java.io.File;
+import java.io.IOException;
+import okio.Okio;
 
 public class OkioActivity extends AppCompatActivity {
 
+    private String txt = "Anything but ordinary";
+    private String filePath = Environment.getExternalStorageDirectory() + "/" + "output.txt";
+    private File file ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_okio);
+//        setContentView(R.layout.activity_constraint_test);
+        writeText();
+    }
+
+    private void writeText() {
+        file = new File(filePath);
+        try {
+            Okio.buffer(Okio.sink(file)).writeUtf8(txt).close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
