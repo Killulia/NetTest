c770ab0436f816fa59842b4f85379dc4d0f5b467
Kingwag <631703900@qq.com>
2018-09-10 13:43:09 +0800

    add
    添加约束布局练习

---
+++ app/src/main/java/com/bksx/nettest/activity/OkioActivity.java
@@ -0,0 +1,13 @@
+package com.bksx.nettest.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class OkioActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_okio);
+    }
+}
