aa861568efdf8666f42f1aefb7a077d79be3cf3b
Kingwag <631703900@qq.com>
2019-03-04 16:41:15 +0800

    lrucache

--- artexploration/src/main/java/com/example/artexploration/part12/BitmapActivity.java
+++ artexploration/src/main/java/com/example/artexploration/part12/BitmapActivity.java
@@ -1,10 +1,13 @@
 package com.example.artexploration.part12;
 
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.support.annotation.NonNull;
-import android.support.v4.util.LruCache;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.util.LruCache;
+import android.widget.ImageView;
 import com.example.artexploration.R;
 
 import java.util.HashMap;
@@ -12,22 +15,32 @@ import java.util.LinkedHashMap;
 
 public class BitmapActivity extends AppCompatActivity {
 
-    LruCache mMemoryCache;
-
+    LruCache<String,Bitmap> mMemoryCache;
+    Bitmap bitmap,bitmap2,bitCache;
+    ImageView imageView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bitmap);
+        imageView = findViewById(R.id.img);
         lruCache();
+        bitmap = BitmapFactory.decodeResource(getResources(), R.mipmap.wade);
+        if (bitmap!=null){
+            Log.d("ccg", "bit not null");
+        }
+        mMemoryCache.put("kingwag", bitmap);
+        bitmap2 = mMemoryCache.get("kingwag");
+        imageView.setImageBitmap(bitmap2);
     }
 
     private void lruCache() {
-        int maxMemory = (int) (Runtime.getRuntime().totalMemory() / 1024);
+        int maxMemory = (int) (Runtime.getRuntime().totalMemory()/1024);
         int cacheSize = maxMemory / 8;
         mMemoryCache = new LruCache<String, Bitmap>(cacheSize){
             @Override
             protected int sizeOf(@NonNull String key, @NonNull Bitmap value) {
-                return value.getByteCount();
+//                return value.getByteCount()/1024;
+                return value.getRowBytes()*value.getHeight()/1024;
             }
         };
 
