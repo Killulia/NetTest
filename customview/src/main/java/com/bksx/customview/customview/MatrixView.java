4987df6c7e86c65803dbf95fe227a9098eff1078
kingwag <631703900@qq.com>
2019-05-07 23:39:50 +0800

    Merge branch 'wwt' of https://github.com/Kingwag/NetTest into wwt
    # Conflicts:
    #	.idea/caches/build_file_checksums.ser
    #	customview/src/main/java/com/bksx/customview/MainActivity.java
    #	customview/src/main/java/com/bksx/customview/Utils.java
    #	customview/src/main/res/layout/activity_main.xml

--- customview/src/main/java/com/bksx/customview/customview/MatrixView.java
+++ customview/src/main/java/com/bksx/customview/customview/MatrixView.java
@@ -15,7 +15,7 @@ import androidx.annotation.Nullable;
 
 public class MatrixView extends View {
 
-    private static final int IMAGE_WIDTH = (int) Utils.dp2px(70);
+    private static final int IMAGE_WIDTH = (int) Utils.dp2px(150);
     private static final int IMAGE_PADDING = (int) Utils.dp2px(80);
 
     Bitmap bitmap;
@@ -35,40 +35,32 @@ public class MatrixView extends View {
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        //让在左上角的图片平移200后绕图片中心旋转45，效果应该是左上角出去屏幕外一点
-        //1.反着写的情况，不考虑坐标系变化，想象是正序先平移200即canvas.translate(200,0)，
-        //再绕图片中心旋转45即 canvas.rotate(45,200+IMAGE_WIDTH/2,IMAGE_WIDTH/2),然后把顺序调过来
-        //得到如下代码
-//        canvas.rotate(45,200+IMAGE_WIDTH/2,IMAGE_WIDTH/2);
-//        canvas.translate(200,0);
-
-        //2.正着顺序，考虑坐标系变化，先将坐标系右移200即canvas.translate(200,0)，让坐标系绕图片中心转
-        //即canvas.rotate(45,IMAGE_WIDTH/2,IMAGE_WIDTH/2)，书写顺序不变得到如下代码
-        //PS:就用第2种方法，不考虑第一种
-//        canvas.translate(200,0);
-//        canvas.rotate(45,IMAGE_WIDTH/2,IMAGE_WIDTH/2);
-//        canvas.drawBitmap(bitmap, 0, 0, paint);
 
 
 //        Camera变换 坐标原点在中央
-        canvas.translate(getWidth() , getHeight() / 2);
+        canvas.translate(getWidth() / 2, getHeight() / 2);
+        //最新理论
+        //ps:以plan B 为例，不要去管camera的位置，首先canvas.translate(getWidth() / 2, getHeight() / 2)将canvas的原点移到
+        //View中心，camera.rotateX(30)会产生一个变换矩阵， camera.applyToCanvas(canvas)将这个变换矩阵付给了
+        //现在的canvas使之旋转，而旋转又是需要旋转中心的，旋转中心就是原点，现在原点在View中心,则将整个canvas
+        //绕view中心做旋转变换，再在这个变换的canvas上画bitmap得到的就是想要的效果。plan A同理，二者实际效果相同
         camera.applyToCanvas(canvas);
         //canvas大小与view的大小一致或者说canvas无边界，最终的大小就是留在View范围内裁去多余的部分
         float w = canvas.getWidth();
         float h = canvas.getHeight();
         float vw = getWidth();
         float vh = getHeight();
-        Log.d("ccg", "canvas大小："+w+","+h);
-        Log.d("ccg", "view大小："+vw+","+vh);
-        canvas.drawBitmap(bitmap, -(IMAGE_WIDTH / 2), -(IMAGE_WIDTH / 2), paint);
-        canvas.translate(-getWidth() / 2, -getHeight() / 2);
+        Log.d("ccg", "canvas大小：" + w + "," + h);
+        Log.d("ccg", "view大小：" + vw + "," + vh);
 
-        //Camera变换 坐标原点默认，即hencoder方式
-        //ps:这种不对，因为canvas平移会带动camera的坐标系平移
-//        canvas.translate(-(getWidth()/2),-(getHeight()/2));
-//        camera.applyToCanvas(canvas);
-//        canvas.drawBitmap(bitmap, getWidth()/2-IMAGE_WIDTH/2, getHeight()/2-IMAGE_WIDTH/2, paint);
-//        canvas.translate(getWidth()/2,getHeight()/2);
+        //plan A 这种是完成旋转后提前把原点移回去，所以画bitmap时为了让他在原来的位置，坐标用移回去的坐标系体系下的
+//        canvas.translate(-(getWidth() / 2), -(getHeight() / 2));
+//        canvas.drawBitmap(bitmap, (getWidth() / 2)-(IMAGE_WIDTH / 2), (getHeight() / 2)-(IMAGE_WIDTH / 2), paint);
+        //plan B 这种是完成旋转后在现在的坐标系下画bitmap,再把坐标系移回去
+        canvas.drawBitmap(bitmap, -(IMAGE_WIDTH / 2), -(IMAGE_WIDTH / 2), paint);
+        canvas.translate(-(getWidth() / 2), -(getHeight() / 2));
+
+
 
 
     }
