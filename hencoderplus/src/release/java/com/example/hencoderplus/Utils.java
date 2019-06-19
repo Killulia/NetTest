97ba85459d159f430e4af9b47a80c9c3cc627946
Kingwag <631703900@qq.com>
2019-06-19 17:58:07 +0800

    gradle sign

---
+++ hencoderplus/src/release/java/com/example/hencoderplus/Utils.java
@@ -0,0 +1,12 @@
+package com.example.hencoderplus;
+
+import android.app.Activity;
+import android.graphics.Color;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Utils {
+    public static void drawBadge(Activity activity) {
+
+    }
+}
