926ba11c9cedf0cb1d6d20fd6ce490ed3e6ca86e
Kingwag <631703900@qq.com>
2019-11-19 17:58:24 +0800

    kotlin-pro

--- hencoderplus/src/main/java/com/example/hencoderplus/kotlin/pro/ProTest.kt
+++ hencoderplus/src/main/java/com/example/hencoderplus/kotlin/pro/ProTest.kt
@@ -1,5 +1,14 @@
 package com.example.hencoderplus.kotlin.pro
 
+import android.view.View
+import com.example.hencoderplus.recyclerview.DemoAdapter
+
+
+//扩展函数，不改变原有的类为其增加函数,比如可以避免写各种XXUtils,都放在一个kt文件的top level
+fun String.expand() = this.length + 1
+//扩展属性
+val String.size:Int
+    get() = this.length
 fun main(){
     val data = Data("1","data")
     val dataCopy = data.copy()
@@ -12,7 +21,20 @@ fun main(){
     val(code,name) = data
     println("解构对象code:$code,name:$name")
 
+    expandTest()
 
+    inTest()
+
+    val view = View()
+    view.setOnclickListener { onClick() }
+
+
+}
+/*
+内敛函数，代码会直接复制到调用处，可反编译验证，适合传入的参数为函数类型时使用
+ */
+inline fun inTest(){
+    println("inline")
 }
 
 fun whenTest(){
@@ -45,4 +67,21 @@ fun lambdaTest(){
 fun companyObject(){
     val application = BaseApplication.currentApplication
     //无法赋值因为set 被private了
+}
+
+fun expandTest(){
+    val str = "test"
+    println(str.expand())
+}
+
+
+
+fun onClick(){
+    println("click")
+}
+
+class View{
+    fun setOnclickListener(listener: (View) -> Unit){
+
+    }
 }
