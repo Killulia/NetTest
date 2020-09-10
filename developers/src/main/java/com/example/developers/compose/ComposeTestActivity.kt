4d7146664c4caa849956a1aad0b9cbf7f6f4e240
Kingwag <631703900@qq.com>
2020-09-10 18:02:58 +0800

    ....

---
+++ developers/src/main/java/com/example/developers/compose/ComposeTestActivity.kt
@@ -0,0 +1,10 @@
+package com.example.developers.compose
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ComposeTestActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
--- hencoderplus/src/main/java/com/example/hencoderplus/view/ViewDrawProcess.java
+++ hencoderplus/src/main/java/com/example/hencoderplus/view/ViewDrawProcess.java
@@ -1,20 +1,37 @@
 package com.example.hencoderplus.view;
 
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.hencoderplus.R;
 
-public class ViewDrawProcess extends AppCompatActivity {
+public class ViewDrawProcess extends Activity {
     TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view);
         textView = findViewById(R.id.tvViewProcess);
-        textView.setText(Thread.currentThread().getName());
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(final View v) {
+                Thread thread = new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        v.requestLayout();
+                        textView.setText(Thread.currentThread().getName());
+                    }
+                });
+                thread.start();
+            }
+        });
+
+
+
 
     }
 }
